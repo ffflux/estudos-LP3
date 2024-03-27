4c0807d657f7a365d2ab083e59ff5542f6772c50
Alison Jorge <127396959+ffflux@users.noreply.github.com>
2024-03-26 23:10:05 -0300

    Delete ex02.py

--- ex02.py
+++
@@ -1,7 +0,0 @@
-# ex02 - Escreva um programa em pyhton que solicita ao usuário três números inteiros e apresenta a média aritmética dos números informados.
-numero1 = int(input('Digite um número inteiro'))
-numero2 = int(input('Digite um número inteiro'))
-numero3 = int(input('Digite um número inteiro'))
-
-media = numero1 + numero2 + numero3 / 3
-print(media)
