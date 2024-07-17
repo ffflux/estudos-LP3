d23df53f7026990d420665636d271117a72e88e8
Alison Jorge <127396959+ffflux@users.noreply.github.com>
2024-07-17 00:50:25 -0300

    Update ex02.py

--- modulos e pacotes/ex02/ex02.py
+++ modulos e pacotes/ex02/ex02.py
@@ -3,8 +3,8 @@ from funcoes import imc, peso_normal, classificacao
 peso = float(input('Digite seu peso em Kg\n'))
 altura = float(input('Digite sua altura\n'))
 
-imc = imc(peso, altura)
-classificacao = classificacao(imc)
-print('Sua classificação é ', classificacao)
-peso_normal = peso_normal(imc)
-peso_normal(imc)
+
+imc_valor = imc(peso, altura)
+classificacao_valor = classificacao(imc_valor)
+print('Sua classificação é', classificacao_valor)
+peso_normal(imc_valor)
