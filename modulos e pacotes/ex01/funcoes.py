8159c27908b1d05b956c25b39f7a43aef3c1211e
Alison Jorge <127396959+ffflux@users.noreply.github.com>
2024-07-17 00:50:07 -0300

    Update funcoes.py

--- modulos e pacotes/ex01/funcoes.py
+++ modulos e pacotes/ex01/funcoes.py
@@ -1,8 +1,8 @@
 def volume(comprimento, altura, largura):
-    (comprimento * altura * largura) / 1000
+    return (comprimento * altura * largura) / 1000
 
 def potenciatermostato(volume, tempdesejada, tempambiente):
-    potencia = volume * 0.05 * (tempdesejada - tempambiente)
+    return volume * 0.05 * (tempdesejada - tempambiente)
 
 def qtdfiltragem(volume):
-    filtragem = volume * 3
+    return volume * 3
